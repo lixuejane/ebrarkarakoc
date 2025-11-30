12d993d4b4b76739f5d1e147467faf36e914415b
lixuejane <ebrar1322@gmail.com>
2025-11-30 17:49:15 +0300

    Package ID guncellendi

--- ui/src/networkConfig.ts
+++ ui/src/networkConfig.ts
@@ -14,7 +14,7 @@ import { createNetworkConfig } from "@mysten/dapp-kit";
  * │ Modules: arena, hero, marketplace
  * └──
  */
-const PACKAGE_ID = "";
+const PACKAGE_ID = "0x34ac751db25ccc049fbd52bd3aedca9e29e390dfd04c1d60093202b39dd69989";
 
 const { networkConfig, useNetworkVariable, useNetworkVariables } =
   createNetworkConfig({
